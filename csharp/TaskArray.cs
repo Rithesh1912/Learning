38acdb3da8eabf81858009f66a99516373fdc8d4
Rithesh <Rithesh1912@gmail.com>
2024-12-31 12:16:30 +0530

    main

---
+++ csharp/TaskArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rithesh
+{
+    internal class TaskArray
+    {
+
+        int[] myarr = { 1, 2, 3, 4, 5, 6, 7 };
+
+        public void DisplayExist()
+        {
+            Console.WriteLine("enter the element that you want to check if it exist or not: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int value = Array.IndexOf(myarr, input);
+
+            //for (int i = 0; i < myarr.Length; i++) {
+            //    {
+            //        if (myarr[i] == input)
+            //        {
+            //            Console.WriteLine("exits");
+            //            break;
+            //        }
+
+
+            //    }
+            //}
+
+            if (value == -1)
+            {
+                Console.WriteLine("not EXIST");
+            }
+            else
+            {
+                Console.WriteLine(" exist");
+            }
+
+        }
+    }
+}
---
+++ csharp/TaskArray2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rithesh
+{
+    internal class TaskArray2
+    {
+        string[] fruitsarr = ["Apple", "Banana", "Watermelon", "Lemon", "Pineapple"];
+
+        public void DisplayFruits()
+        {
+            Console.WriteLine("enter the fruit name to find: ");
+            string input = Console.ReadLine();
+
+            int value = Array.IndexOf(fruitsarr, input);
+
+            //char a = value[0];
+            //a = char.ToUpper(a);
+
+
+
+            if (value == -1)
+            {
+                Console.WriteLine("fruit is not present");
+            }
+            else
+            {
+                Console.WriteLine("fruit is present");
+            }
+        }
+    }
+}
